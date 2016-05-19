1a2cb2905c2eb4e944d2dc0ede0a6172906cd2f7
dalmo@osx <kristian@doingit.no>
2016-05-20 01:42:54 +0200

    Added missing namespace imports for head, tail and weighted.mean.

--- R/tabulR.R
+++ R/tabulR.R
@@ -4,6 +4,8 @@
 #' that uses \code{\link[data.table]{data.table}} to generate a quick margin table for one
 #' or more variables, by one or more groups. See \code{\link{qtable}} for more details.
 #'
+#' @importFrom stats weighted.mean
+#' @importFrom utils head tail
 #' @author Kristian D. Olsen
 #' @docType package
 #' @name tabulR

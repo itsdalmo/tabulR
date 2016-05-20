2e6644d6a9eab266914dd4b918de4e8dcf1a2efd
dalmo@pwin <kristian@doingit.no>
2016-05-20 15:11:09 +0200

    Updated package documentation now that it includes plots as well.

--- R/tabulR.R
+++ R/tabulR.R
@@ -1,8 +1,8 @@
 #' tabulR/qtable: Create "quick" tables.
 #'
-#' This is a tiny package with two functions (named \code{qtable} and \code{qtable_})
-#' that uses \code{\link[data.table]{data.table}} to generate a quick margin table for one
-#' or more variables, by one or more groups. See \code{\link{qtable}} for more details.
+#' This is a tiny package that uses \code{\link[data.table]{data.table}} to generate
+#' a quick margin table or plot for one or more variables, by one or more groups.
+#' See \code{\link{qtable}}, \code{\link{bar_chart}} and \code{\link{line_chart}} for more details.
 #'
 #' @importFrom stats weighted.mean
 #' @importFrom utils head tail
